5541af08e610504004a37578a51474d020934ecc
star02046 <ohdarkashish123@gmail.com>
2024-03-04 20:54:24 +0530

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -24,13 +24,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        if (a%2==0){
-            System.out.println("Even no");
-
+        if (a==b){
+            System.out.println("Equal");
         }
-        else {
-            System.out.println("Odd no");
+        else
+        {
+            if (a>b){
+                System.out.println("A is greater");
+            }
+            else {
+                System.out.println("A is smaller");
+            }
         }
     }
 }
